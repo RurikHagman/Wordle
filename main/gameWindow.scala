7681c55e6c996fe05767148db4f9c51d9a2de534
RurikHagman <112903317+RurikHagman@users.noreply.github.com>
2022-11-22 12:03:24 +0100

    offset adjustment

--- main/gameWindow.scala
+++ main/gameWindow.scala
@@ -13,7 +13,7 @@ val window = introprog.PixelWindow(7 * blockSize, 11 * blockSize, "Wordle", java
 def drawLetterBlock(pos: (Int, Int), letter: Char, state: java.awt.Color, size: Int): Unit = 
     val x = pos(1) * (blockSize - size)
     val y = pos(0) * (blockSize - size)
-    val offsetMap: Map[Char, Int] = Map('Q' -> 9, 'W' -> 6, 'E' -> 10, 'R' -> 9, 'T'-> 10, 'Y' -> 10, 'U' -> 10, 'I' -> 18, 'O' -> 8, 'P' -> 10, 'A' -> 8, 'S' -> 10, 'D' -> 10, 'F' -> 12, 'G' -> 7, 'H' -> 8, 'J' -> 13, 'K' -> 10, 'L' -> 12, 'Z' -> 10, 'X' -> 9, 'C' -> 7, 'V' -> 10, 'B' -> 10, 'N' -> 9, 'M' -> 8)
+    val offsetMap: Map[Char, Int] = Map('Q' -> 9, 'W' -> 6, 'E' -> 10, 'R' -> 9, 'T'-> 10, 'Y' -> 10, 'U' -> 10, 'I' -> 20, 'O' -> 8, 'P' -> 10, 'A' -> 8, 'S' -> 10, 'D' -> 10, 'F' -> 12, 'G' -> 7, 'H' -> 8, 'J' -> 13, 'K' -> 10, 'L' -> 12, 'Z' -> 10, 'X' -> 9, 'C' -> 7, 'V' -> 10, 'B' -> 10, 'N' -> 9, 'M' -> 8)
     window.fill(x, y , (blockSize - size) , (blockSize - size) , State.Idle)
     window.fill((x + 3), (y + 3), ((blockSize - size) - 6), ((blockSize - size) - 6), java.awt.Color(110,110,110))
     window.fill((x + 6), (y + 6), ((blockSize - size) - 12), ((blockSize - size) - 12), state)
