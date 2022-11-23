63bed5679fc2649af3e88bb1b02ce9d08c309e92
RurikHagman <112903317+RurikHagman@users.noreply.github.com>
2022-11-23 10:18:58 +0100

    fixed dictionary content

--- main/Main.scala
+++ main/Main.scala
@@ -4,17 +4,15 @@ package wdl
     val defaultN = 10
 
     var urlText = Text.fromURL(defaultUrl)
-
     val dictionary = Text.fromFile(introprog.IO.currentDir() + """\""" + "dictionary.txt")
-
     var text: Text = Text.fromFile(introprog.IO.currentDir() + """\""" + "words.txt")
 
     val word: String = text.randomElem((text.wordsOfLength(5)).toVector)
-    
     val game = wordleGame(word)
 
     def main(args: Array[String]): Unit =
 
+      // test dictionary content: text.words.foreach(s => if !dictionary.words.contains(s) then println(s))
       game.play()
       
       
